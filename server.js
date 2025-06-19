9c62bc0e2083fbd81c8babdf631e7604667bfbf2
ashish-0280 <ashishsingh5160280@gmail.com>
2025-06-20 01:08:49 +0530

    new

--- server.js
+++ server.js
@@ -15,5 +15,4 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
-console.log("Hi, I am here")
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
